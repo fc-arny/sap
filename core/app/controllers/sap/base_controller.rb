6f56a316afe978e9cee5972ee9e5443f7f7e5cca
fc_arny <arthur.shcheglov@gmail.com>
2013-08-06 00:11:03 +0400

    Test sub repo

--- core/app/controllers/sap/base_controller.rb
+++ core/app/controllers/sap/base_controller.rb
@@ -3,13 +3,9 @@
 # -------------------------------------------------------------
 class Sap::BaseController < ApplicationController
 
-
-
   private
 
   #def miniprofiler
   #  Rack::MiniProfiler.authorize_request # if user.admin?
   #end
-
-
 end
